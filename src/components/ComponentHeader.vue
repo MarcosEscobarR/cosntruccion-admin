49163fa67b85b6312c7f9e0ef06c953e5793ba56
MarcosEscobarR <marcosmaer600@gmail.com>
2021-04-08 23:06:37 -0400

    first commit

---
+++ src/components/ComponentHeader.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ComponentHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/construction/ConstructionIndex.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <div>
+      <breadcrumbs :items="breadItems"/>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Breadcrumbs from '../../components/Breadcrumbs'
+import { BreadCrumbsModel } from '@/models/BreadCrumbsModel'
+
+export default Vue.extend({
+  name: 'index-construction',
+  components: { Breadcrumbs },
+  data: () => ({
+    breadItems: [{
+      text: 'Obras',
+      disabled: true,
+      href: 'home'
+    }] as BreadCrumbsModel[]
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
